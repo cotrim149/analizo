ac4e3e6810b50dcee8ec3f738f75559824f63fca
KamillaaaH <holanda.kamilla@gmail.com>
2013-06-12 08:57:22 -0300

    Refactoring yaml2csv module

--- lib/Analizo/Batch/Output/yaml2csv.pm
+++ lib/Analizo/Batch/Output/yaml2csv.pm
@@ -2,123 +2,76 @@ use strict;
 use warnings;
 package Analizo::Batch::Output::yaml2csv;
 
-
 sub new 
 {
   my ($class, @yaml_file) = @_;
   return bless {file_name => @yaml_file }, $class;
 }
 
-
 sub extract_labels
 {
   my ($self) = @_;
   my @labels = ();
+  my $line = "";
 
-  open(my $yaml_handler, "<", $self->{file_name}) || return 0;
+  open(my $yaml_handler, "<", $self->{file_name}) || die "Couldn't open '".$self->{file_name}."' for reading because: ".$!;
 
-  while(!eof $yaml_handler)
-  { 
-    my $line = readline $yaml_handler;
-    if($line ne "---\n" and $line =~ m/(\w+):/)
+while(!eof $yaml_handler) { 
+  my $line = readline $yaml_handler; 
+  if($line ne "---\n" and $line =~ m/(\w+):/)
     {
-      push @labels, $1;
+      
       if($1 eq "sc")
       {
+        push @labels, "$1\n";
 	close $yaml_handler;
-	last;
+        last;
+      }else
+      {
+	push @labels, $1;
       }
+      
     }
-  }
-    
+}
   return @labels;
 }
 
 sub extract_lines
 {
 
-  my ($self) = @_;
+  my ($self, $arrSize) = @_;
   my @lines = ();
-  open(my $yaml_handler, "<", $self->{file_name}) || return 0;
-	
-  while(!eof $yaml_handler)
-  {
-    my $line = readline $yaml_handler;
-    if($line ne "---\n" and $line =~ m/( (\d+)| (\w+)| - (\w+).(\w+))/)
+  my $i = 0;
+  
+  open(my $yaml_handler, "<", $self->{file_name}) || die "Couldn't open '".$self->{file_name}."' for reading because: ".$!;
+  while(!eof $yaml_handler){ 
+    my $line = readline $yaml_handler; 
+    if(($line ne "---\n") and ($line =~ m/($\d+| \d+\.\d+| \w+|- \w+.\w+)/))
     {
-      push @lines, $1;
+     
+      if($i == ($arrSize -1))
+      {
+	push @lines, "$1\n";
+      }else
+      {
+	push @lines, $1;
+      }
+      $i++;
     }
-  } 
-  close $yaml_handler;
-  return @lines;
+  }
+    close $yaml_handler;
+    return @lines;
 }
 
 sub write_csv 
 {
   my ($self) = @_;
-  my @labels = $self->extract_labels(); 
-  my @lines = $self->extract_lines();
-  # Nome do arquivo
-  my $csvfile = $self->{file_name};
-  # Abre o arquivo
-  # $csv_handler file handler do arquivo
+  open(my $csv_handler, ">", "saida.csv") || die "nao pode abrir saida.csv".$!;
+
+  my $arrSize = $self->extract_labels();
+  print $csv_handler join(",", $self->extract_labels());
+  print $csv_handler join(",", $self->extract_lines($arrSize));
   
-  my @csv_result = ();
-  # Variavel onde as linhas serão escritas
-  my $csv_line = "";
-  # Contador para auxiliar
-  my $i = 0;
-  # Quantidade de valores por linha
-  my $number_of_values = scalar @labels;
-
-  open(my $csv_handler, ">", $csvfile) || die "Couldn't open '".$csvfile."' for reading because: ".$!;
-  
-
-  # Para cada elemento no array de colunas
-  foreach (@labels){
-  # A primeira linha não possui virgula na frente
-    if($i == 0){
-     $csv_line = $_; # Escreve o primeiro elemeto da linha
-     $i = 1;
-    }
-    else{
-     # Concatena os outros valores da linha com a virgula entre eles
-     $csv_line = $csv_line . "," . $_;
-    }
-  }
-  # Ao terminar de tratar a primeira linha adiciona ela no array de linhas a serem escritas no arquivo
-  push @csv_result, $csv_line;
-
-  # Reinicializa o $i
-  $i = 0;
-  # Começa a tratar as linhas
-  foreach (@lines){
-    # No primeiro valor da linha só adiciona, sem virgula
-    if($i == 0){
-      $csv_line = $_;
-      $i++;
-    } else {
-    # Concatena os elementos com virgula entre eles
-    $csv_line = $csv_line . "," . $_;
-    $i++;
-    # Quando chega no ultimo elemento de acordo com o numero de colunas
-    if($i==$number_of_values){
-       # Adiciona a linha para o array de linhas a serem escritas no arquivo
-       push @csv_result, $csv_line;
-       # Reinicializa o $i para a próxima linha
-       $i = 0;
-      }
-    }
-  }
-
-  # Para cada linha a ser escrita no arquivo
-  foreach (@csv_result){
-    # Escreve a linha no arquivo
-    print $csv_handler $_;
-    print $csv_handler "\n";
-  }
-
-  # Fecha o file handler
   close $csv_handler;
   return 1;
 }
