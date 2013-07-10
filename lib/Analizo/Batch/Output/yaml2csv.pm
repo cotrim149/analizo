cc3571ff3f780baa4494ebe523bfb31f44c103e5
KamillaaaH <holanda.kamilla@gmail.com>
2013-07-10 11:37:03 -0300

    Module yaml2csv working properly

--- lib/Analizo/Batch/Output/yaml2csv.pm
+++ lib/Analizo/Batch/Output/yaml2csv.pm
@@ -23,8 +23,8 @@ sub extract_labels
       push @labels, $1;
       if($1 eq "sc")
       {
-	close $yaml_handler;
-	last;
+  close $yaml_handler;
+  last;
       }
     }
   }
@@ -35,44 +35,34 @@ sub extract_labels
 
 sub extract_lines
 {
-
   my ($self, $number_of_labels) = @_;
-  my @array_of_values = ();
   my @values = ();
-  my @temp_line = ();
-  open(my $yaml_handler, "<", $self->{file_name} . "-details.yml")  || return 0;
+  my @array_of_values = ();
+  my @files_names = ();
 
-  while(!eof $yaml_handler)
+  open(my $yaml_handler, "<", $self->{file_name} . "-details.yml")  || return 0;  
+  
+  while(my $line = readline $yaml_handler)
   {
-    my $line = readline $yaml_handler;
-    if($line ne "---\n" and $line =~ m/( (\d+)| (\w+)| - (\w+).(\w+))/)
+    if($line =~ m/( .*)/)
     {
-      push @temp_line, $1;
-    }
-    elsif (@temp_line)
-    {
-      # identifica quantos arquivos possui
-      my $number_of_files = scalar(@temp_line)  - ($number_of_labels + 1);
-      my @file_names = ();
-      # armazena os arquivos em um vetor temp
-      for(my $i = 0; $i < $number_of_files; $i++)
+      if($1 =~ m/(- (.*))/ )
       {
-        push @file_names, $temp_line[$i];
+        push @files_names, $1." ";
       }
-      # transforma os arquivos em uma variavel só
-      my $files = join(" ", @file_names);
-      # coloca os arquivos na primeira posição do vetor
-      push @values, $files;
-
-      #adiciona o restante dos valores
-      for(my $i=($number_of_files - 1);$i < scalar(@temp_line);$i++)
+      else
       {
-        push @values, $temp_line[$i];
+        push @values, $1;  
       }
-
-      #coloca o @values no @array_of_values, pois @values são os valores de uma linha
-      push @array_of_values, join(",",@values);
-      @temp_line = ();
+      if($number_of_labels == ((@values)+1))
+      { 
+        push @array_of_values, @files_names;
+        push @array_of_values, ",";
+        push @array_of_values, join(",", @values);
+        push @array_of_values, "\n";
+        @values = ();
+        @files_names = ();
+      }
     }
   } 
   close $yaml_handler;
@@ -84,23 +74,17 @@ sub write_csv
   my ($self) = @_;
   my $csv_filename = $self->{file_name} . "-details.csv";
   open my $csv_handler, '>'.$csv_filename  || die "can not open ".$self->{file_name} . "-details.csv\n".$!;
-  #open(my $csv_handler, ">", $self->{file_name} . "-details.csv") || die "can not open ".$self->{file_name} . "-details.csv\n".$!;
-
-  my @temp = ();
 
   my $number_of_labels = $self->extract_labels();
   print $csv_handler join(",", $self->extract_labels());
   print $csv_handler "\n";
 
   my @array_of_values =  $self->extract_lines($number_of_labels);
-  # foreach(@array_of_values)
-  # {
-  #   print $csv_handler $_;
-  # }
-  print $csv_handler join("\n", @array_of_values);
+  foreach(@array_of_values)
+  {
+     print $csv_handler $_;  
+  }
 
- 
- 
   close $csv_handler;
 
   return 1;
