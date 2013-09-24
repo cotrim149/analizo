94ee5298105da0581a850de49d659c8a4775c32e
BryanFernandes <bryan.h.fernandes@hotmail.com>
2013-09-24 01:57:36 -0300

    Refactoring write_data the function (): Creating the function write_details (). The behavior of direct writing in output file (csv extension) is now performed by write_details function () as the orientation Terceiro. (Note: all metrics-batch are written, but not organized).
    Signed-off-by: Bryan de Holanda <bryan.h.fernandes@hotmail.com>

--- lib/Analizo/Batch/Output/CSV.pm
+++ lib/Analizo/Batch/Output/CSV.pm
@@ -36,6 +36,8 @@ sub write_data {
     my $line = join(',', $job->id, @metadata, @values) . "\n";
     print $fh $line;
 
+	$self->write_details($job->id, $details);
+
     #print "Writing details file for ... ",  $job->directory, "\n";
     #my $details_yaml = $job->directory . "-details.yml";
     #open DETAILS, '>' . $details_yaml;
@@ -45,12 +47,7 @@ sub write_data {
     #my $yaml2csv = Analizo::Batch::Output::yaml2csv->new($job->directory);
     #$yaml2csv->write_csv();
 
-	my $csv_filename = "bla" .$count.  "-details.csv";
-
-	open my $csv_handler, '>'.$csv_filename  || die "Cannot open bla" .$count. "-details.csv\n".$!;
-
-	print $csv_handler join('', map { Dump($_)} @$details);
-  	close $csv_handler;
+	
 
 	$count++;
   }
@@ -66,6 +63,31 @@ sub _encode_value($) {
   my ($value) = @_;
   my $encoder = $__encoders->{ref($value)} || $__encoders->{_default};
   return &$encoder($value);
+}
+
+sub write_details {
+	my ($self, $id, $details) = @_;
+
+	my $csv_filename = "bla" .$count.  "-details.csv";
+	#my $csv_filename = $self->{job_directory} . "-details.csv";
+
+	open my $csv_handler, '>'.$csv_filename  || die "Cannot open bla" .$count. "-details.csv\n".$!;
+	#open my $csv_handler, '>'.$csv_filename  || die "Cannot open ".$self->{job_directory} . "-details.csv\n".$!;
+
+	print $csv_handler join('', map { Dump($_)} @$details);
+  	close $csv_handler;
+
+  #my $number_of_labels = $self->extract_labels();	#*HERE*
+  #print $csv_handler join(",", $self->extract_labels());	#*HERE*
+  #print $csv_handler "\n";
+
+  #my @array_of_values =  $self->extract_lines($number_of_labels);	#*HERE*
+  #foreach(@array_of_values)
+  #{
+   #  print $csv_handler $_;  
+  #}
+
+  #close $csv_handler;
 }
 
 1;
