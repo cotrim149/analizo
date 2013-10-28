b64be6b9ed2ffe617f43220cc08b9e0ec91ff3d8
Cotrim <cotrim149@gmail.com>
2013-10-27 22:56:01 -0200

    Putting sample of Perl

---
+++ t/samples/animals/perl/Cat.pm
@@ -0,0 +1,16 @@
+package Cat;
+
+use base qw(Mammal);
+use strict;
+use warnings;
+
+
+sub name {
+	my $self = shift;
+	
+	$self->{name} = shift if (@_);
+	
+	return $self->{name};
+}
+
+1;
---
+++ t/samples/animals/perl/Mammal.pm
@@ -0,0 +1,15 @@
+package Mammal;
+
+use strict;
+use warnings;
+
+sub new {
+	my $class = shift;
+	
+	my $self = {name=>shift}; 
+		
+	bless ($self,$class);
+	return $self;
+}
+
+1;
