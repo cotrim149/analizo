ea56b80afaaa65d33d435a5002c605abcb57032f
Cotrim <cotrim149@gmail.com>
2013-11-10 17:24:42 -0200

    Correct bugs

--- lib/Analizo/Extractor/Bxref/Tree.pm
+++ lib/Analizo/Extractor/Bxref/Tree.pm
@@ -14,10 +14,12 @@ sub add_method_to_tree {
 	my ($self, $file, $element_method, $element_usage, $element_name, $element_type, $element_package) = @_;
 
 	if ($element_usage =~ /^subused$/ && !($element_type =~ /\?/)) {
-		if (!($element_package =~ /main/ || $element_package =~ /(lexical)/))
-			$tree->{$file}->{$element_method}->{"called_methods"}->{$element_name} = $element_package;
-		else
-			$tree->{$file}->{$element_method}->{"called_methods"}->{$element_name} = $self->father_seeker($element_name);
+		if (!($element_package =~ /main/ || $element_package =~ /(lexical)/)){
+			$tree->{$file}->{$element_method}->{"called_methods"}->{$element_name} = $element_package;		
+		}
+		else{
+			$tree->{$file}->{$element_method}->{"called_methods"}->{$element_name} = $self->father_seeker($element_name);		
+		}
 	}
 }
 
@@ -30,12 +32,13 @@ sub father_seeker {
 		$files = $tree->{@_};
 
 		foreach (keys %$files) {
-			if (@_ =~ $method_name) {
+			if ($_ =~ /$method_name/) {
 				$methods = $files->{@_} unless (/global_variables/);
 
 				foreach (keys %$methods) {
-					if (@_ =~ /method_parent/) 
-						return ($methods->{@_});
+					if ($_ =~ /method_parent/) {
+						return ($methods->{@_});					
+					}
 				}
 			}
 		}
@@ -57,8 +60,8 @@ sub building_tree {
 
 	#FIX FILE NAMES
 
-	#									$1			$2			$3		$4			$5						$6				$7
-	#									file    method  line  package -----type---  name    ---used----  
+	#	$1		$2		$3		$4			$5		$6			$7
+	#	file    method  line  package -----type---  name    ---used----  
 	if ($lines =~ /^(\S+)\s+(\S+)\s+(\d+)\s(\S+)\s+([@*&\$%?>-]+)\s(\S+)\s+([a-zA-Z]+)$/) {
 		$file = $1;
 		$element_method = $2;
@@ -70,7 +73,7 @@ sub building_tree {
 
 		#FIX FILE NAMES
 
-		$tree->{$file}->{"global_variables"} = 0 if (!defined ($tree->{$file}) && grep {$_ eq $file} @files};
+		$tree->{$file}->{"global_variables"} = 0 if (!defined ($tree->{$file}) && grep {$_ eq $file} @files);
 
 		if (grep {$_ eq $file} @files) {
 			my @method_full_name = split "::", $element_method;
@@ -79,14 +82,17 @@ sub building_tree {
 			if ($element_usage =~ /^intro$/) {
 				push @{$tree->{$file}->{$element_method}->{"local_variable_names"}}, $element_name;
 
-				if ($element_method =~ /\(/) 
-					$tree->{$file}->{"global_variables"} ++;
+				if ($element_method =~ /\//){
+					$tree->{$file}->{"global_variables"} ++;				
+				} 
 			} else {
 				if ($element_line != 0 && $element_usage =~ /^used$/) {
-					if (grep{$_ eq $element_name} @{$tree->{$file}->{$element_method}->{"local_variable_names"}})
-						$tree->{$file}->{$element_method}->{"used_variable_names"}->{$element_name} = 0;
-					else
-						$tree->{$file}->{$element_method}->{"used_variable_names"}->{$element_name} = 1;
+					if (grep{$_ eq $element_name} @{$tree->{$file}->{$element_method}->{"local_variable_names"}}){
+						$tree->{$file}->{$element_method}->{"used_variable_names"}->{$element_name} = 0;					
+					}
+					else{
+						$tree->{$file}->{$element_method}->{"used_variable_names"}->{$element_name} = 1;					
+					}
 				}
 
 				if ($element_usage =~ /^subdef$/ && $element_line != 0) {
@@ -96,7 +102,7 @@ sub building_tree {
 				}
 			}
 			
-			$self->add_method_to_tree ($file, $element_method, $element_usage, $element_name, $element_type, $element_package;
+			$self->add_method_to_tree ($file, $element_method, $element_usage, $element_name, $element_type, $element_package);
 		}
 	}
 	
@@ -104,4 +110,3 @@ sub building_tree {
 }
 
 1;
-
